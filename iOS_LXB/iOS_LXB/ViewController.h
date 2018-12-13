a99e0fdf34c4e20df402f1c5c66c7a5643e7b728
Moclin-C <moclin1230@gmail.com>
2018-12-13 18:15:28 +0800

    上传

---
+++ iOS_LXB/iOS_LXB/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOS_LXB
+//
+//  Created by 穗金所 on 2018/12/13.
+//  Copyright © 2018年 kodbin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ iOS_LXB/iOS_LXB/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  iOS_LXB
+//
+//  Created by 穗金所 on 2018/12/13.
+//  Copyright © 2018年 kodbin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
